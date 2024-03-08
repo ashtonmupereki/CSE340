2d8f3a040c3f261b1fd67773f5758d45ae3aee4c
ashtonmupereki <muperekiashton25@gmail.com>
2024-03-08 14:08:46 +0200

    added

---
+++ database/assignment2.sql
@@ -0,0 +1,21 @@
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+DELETE FROM account
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+DELETE FROM account
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+SELECT inv_make, inv_model, c.classification_name
+FROM inventory i
+INNER JOIN classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+UPDATE inventory
+SET inv_image = CONCAT(SUBSTRING(inv_image, 1, 8), '/vehicles', SUBSTRING(inv_image, 9)),
+    inv_thumbnail = CONCAT(SUBSTRING(inv_thumbnail, 1, 8), '/vehicles', SUBSTRING(inv_thumbnail, 9));
