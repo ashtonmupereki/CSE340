3d3d37f7768321c9d55b03496628027ef049da81
ashtonmupereki <muperekiashton25@gmail.com>
2024-03-11 11:28:36 +0200

    added

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,7 +1,8 @@
 INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
-DELETE FROM account
+UPDATE account
+SET account_type = 'Admin'
 WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
 
 DELETE FROM account
